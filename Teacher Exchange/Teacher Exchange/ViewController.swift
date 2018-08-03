e526ae2718dbd0ea69e6b664d906b1ab5d5131dc
Arunav Gupta <centaurus.ab@gmail.com>
2018-08-03 12:54:52 -0700

    create Xcode project

---
+++ Teacher Exchange/Teacher Exchange/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Teacher Exchange
+//
+//  Created by Arunav Gupta on 8/3/18.
+//  Copyright © 2018 ArunavGupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
